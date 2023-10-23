b386c9c3b0160447f6b0c07625fd707b87b46144
ezriharmusial <ezriharmusial@gmail.com>
2023-10-23 20:27:43 +0200

    Seo Constants & Order

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ArtKidsFoundation.org - ';
-export const SITE_DESCRIPTION = 'Artkids Foundation is connecting Musical Talents, Managers, Promoters, Marketers and ICT/Media professionals together so as to reach the world with vital messages.';
+export const SITE_DESCRIPTION = 'Artkids Foundation is a sister organization of ArtKids International. While ArtKids Foundation is a Not-For-Profit humanitarian Organization, ArtKids International is a fully in-Holland registered Business outfit through which the needed operational costs and fund is raised.';
