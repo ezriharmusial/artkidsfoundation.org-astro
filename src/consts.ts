2ef25503044e7fcb941dc5f34d7170e2857ab2a3
ezriharmusial <ezriharmusial@gmail.com>
2024-01-10 07:47:41 +0100

    ANBI

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_URL = 'https://artKidsfoundation.org';
 export const SITE_TITLE = 'ArtKidsFoundation.org - ';
 export const SITE_DESCRIPTION = 'Artkids Foundation is a sister organization of ArtKids International. While ArtKids Foundation is a Not-For-Profit humanitarian Organization, ArtKids International is a fully in-Holland registered Business outfit through which the needed operational costs and fund is raised.';
